a800614f0bf0210a69696d757d7beb5af097be79
Sofie Budman <changbudmanfamily@gmail.com>
2025-05-08 14:11:59 -0700

    africa!!

--- Map.java
+++ Map.java
@@ -11,13 +11,24 @@ public class Map {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("SVG Viewer");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+            frame.setLayout(new GridLayout(1, 2));
             frame.setSize(800, 600);
 
-            // Batik SVG canvas
-            JSVGCanvas svgCanvas = new JSVGCanvas();
-            svgCanvas.setURI(new File("SVGs/usa-canada.svg").toURI().toString());
+            JSVGCanvas canvas1 = new JSVGCanvas();
+            canvas1.setURI(new File("SVGs/usa-canada.svg").toURI().toString());
+            canvas1.setPreferredSize(new Dimension(400, 400));
 
-            frame.getContentPane().add(svgCanvas, BorderLayout.CENTER);
+            // Second canvas
+            JSVGCanvas canvas2 = new JSVGCanvas();
+            canvas2.setURI(new File("SVGs/Africa.svg").toURI().toString());
+            canvas2.setPreferredSize(new Dimension(400, 400));
+            
+
+            frame.add(canvas1);
+            frame.add(canvas2);
+
+            frame.pack();
             frame.setVisible(true);
         });
     }
