a8c7aac9244a9c1290011e646b39b553b1a343e9
Sofie Budman <changbudmanfamily@gmail.com>
2025-05-29 21:24:11 -0700

    organize Notificiation

--- src/main/java/code/Notification.java
+++ src/main/java/code/Notification.java
@@ -11,21 +11,31 @@ public class Notification {
     private static final long DISPLAY_DURATION = 3000; // Display for 3 seconds
     
     public Notification(PApplet p) {
-        this.p = p;
+        Notification.p = p;
         currentMessage = "";
         displayStartTime = 0;
     }
-
+    /*
+     * Creates a new notification witht he miessage
+     * Sets the display time to 30 seconds
+     * Includes the current day
+     */
     public static void newNotification(String message) {
         currentMessage = message;
         displayStartTime = System.currentTimeMillis();
-        Log.addMessage(message, Map.getDay()); // Assuming Map.getDay() returns the current day
+        Log.addMessage(message, Map.getDay());  //adds the message to log
 
     }
 
+
+    /**
+     * Displays current message if it's valid
+     * ensures that message has been shown for under 30 seconds
+     * Draws the notification
+     */
     public static void display() {
         if (currentMessage != null && !currentMessage.isEmpty()) {
-            long currentTime = System.currentTimeMillis();
+           long currentTime = System.currentTimeMillis();
             if (currentTime - displayStartTime < DISPLAY_DURATION) {
                 p.fill(0, 0, 0, 180);
                 p.rect(1100, 55, 330, 30);
@@ -35,7 +45,7 @@ public class Notification {
                 p.textFont(mono);
                 p.text(currentMessage, 1115, 77);
             } else {
-                currentMessage = ""; // Clear the message after duration
+                currentMessage = ""; 
             }
         }
     }
