bfccbd34d4e17bc4ac0d82a846855e9d32cf4432
Sofie Budman <changbudmanfamily@gmail.com>
2025-05-29 21:58:15 -0700

    organize log.java

--- src/main/java/code/Log.java
+++ src/main/java/code/Log.java
@@ -13,7 +13,7 @@ public class Log {
 
     private static boolean showLog;
     private static Textarea logText;
-    private static ArrayList<String> logMessages = new ArrayList<>();
+    private static ArrayList<String> logMessages = new ArrayList<>(); //arraylist of all log messages
 
     private static PFont logFont;
     private static PApplet p;
@@ -23,35 +23,39 @@ public class Log {
         Log.p = p;
         cp5 = new ControlP5(p);
 
-        // Load a monospaced font for log
+
         logFont = p.createFont("Courier New", 12);
 
-        // Create a scrollable Textarea
+        //creates scrollable text area using contorl p5
         logText = cp5.addTextarea("logText")
                 .setPosition(20, 70)
                 .setSize(260, 610)
                 .setFont(logFont)
                 .setLineHeight(14)
-                .setColorBackground(p.color(255)) // white background
-                .setColor(p.color(0)) // text color
-                .setColorForeground(p.color(0)) // border color
+                .setColorBackground(p.color(255)) 
+                .setColor(p.color(0)) 
+                .setColorForeground(p.color(0)) 
                 .setText(getLogMessage())
-                .setVisible(false); // initially hidden
+                .setVisible(false); 
     }
 
+    //toggle visibility
     public static void toggle() {
         showLog = !showLog;
         if (logText != null) {
             logText.setVisible(showLog);
         }
     }
-
+    /**
+     * add each log message to a long string 
+       */ 
+    
     public static String getLogMessage() {
-        StringBuilder out = new StringBuilder();
+        String out = "";
         for (String s : logMessages) {
-            out.append(s).append("\n");
+            out += (s + "\n");
         }
-        return out.toString();
+        return out;
     }
 
     public static void hide() {
@@ -61,6 +65,11 @@ public class Log {
         }
     }
 
+
+    /**
+     * 
+     * add messages to the scrollable text, will exclude certain messages
+     */
     public static void addMessage(String message, int day) {
         if (message.toLowerCase().contains("wait") &&
             message.toLowerCase().contains("days to toggle this border")) {
@@ -71,7 +80,9 @@ public class Log {
             logText.setText(getLogMessage());
         }
     }
-
+    /**
+     * Display log when applicable
+     */
     public static void displayLog() {
         if (!showLog) return;
 
